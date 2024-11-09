854a216fb15729f0ca1a2dcfe5066de561795eb0
Fedechkin Aleksei <alex@centralhardware.me>
2024-11-10 00:44:07 +0100

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    kotlin("jvm") version "2.0.21"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "me.centralhardware.telegram"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation("dev.inmo:tgbotapi:20.0.0")
+    implementation("com.github.centralhardware:telegram-bot-commons:d96b131958")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("shadow")
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to "MainKt"))
+        }
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package me.centralhardware.telegram
+
+import dev.inmo.micro_utils.common.Warning
+import dev.inmo.tgbotapi.longPolling
+
+@OptIn(Warning::class)
+suspend fun main() {
+    longPolling {
+
+    }
+}
